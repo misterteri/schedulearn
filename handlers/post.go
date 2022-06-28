86d29b7e948063bb064f894ef01a18dc6e2ab4d9
misterteri <marcelinogilbert26@gmail.com>
2022-06-28 16:32:46 +0800

    Add: Succesfully unmarshall the message

--- handlers/post.go
+++ handlers/post.go
@@ -1,13 +1,20 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Post struct {
 	Logger *log.Logger
+}
+
+type Message struct {
+	Message string `json:"message"`
 }
 
 func NewPost(logger *log.Logger) *Post {
@@ -17,13 +24,36 @@ func NewPost(logger *log.Logger) *Post {
 func (post *Post) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK) // 200 OK
 
-	// read message.json
-	message, err := ioutil.ReadFile("message.json")
+	// open json file
+	jsonFile, err := os.Open("message.json")
 	if err != nil {
 		post.Logger.Println(err)
 		return
 	}
+	fmt.Println("Successfully Opened message.json")
+	// defer the closing of our jsonFile
+	defer jsonFile.Close()
 
-	// write the body of the message to the log
-	post.Logger.Println("File read: ", string(message))
+	// read our opened jsonFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var message Message
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above.
+	json.Unmarshal(byteValue, &message)
+	// we then print out the content of Message which is
+	// a slice of type Message.
+	post.Logger.Println(message.Message)
 }
+
+// // read message.json
+// message, err := ioutil.ReadFile("message.json")
+// if err != nil {
+// 	post.Logger.Println(err)
+// 	return
+// }
+// // parsing message.json
+// err = json.Unmarshal(message, &message)
+// if err != nil {
+// 	post.Logger.Println(err)
+// 	return
+// }
