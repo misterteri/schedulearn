21c49a169b6e32f586988936921bafe92a7dd74e
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-08-02 11:42:08 +0800

    feat: run trainings in background with fastapi

--- api/main.py
+++ api/main.py
@@ -2,18 +2,37 @@ import logging
 from logging.config import dictConfig
 from dotenv import load_dotenv
 import config
+import docker
+import datetime
 import database as db
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 from pydantic import BaseModel
 from sqlmodel import Session, select, col
 import uvicorn
-from tasks import run_horovod
 
 load_dotenv()
 dictConfig(config.LOGGING)
 
 logger = logging.getLogger("schedulearn")
 app = FastAPI(debug=True)
+
+client3 = docker.DockerClient(
+    base_url=config.GPU3_DOCKER_HOST,
+    use_ssh_client=True,
+    tls=True,
+)
+
+client4 = docker.DockerClient(
+    base_url=config.GPU4_DOCKER_HOST,
+    use_ssh_client=True,
+    tls=True,
+)
+
+client5 = docker.DockerClient(
+    base_url=config.GPU5_DOCKER_HOST,
+    use_ssh_client=True,
+    tls=True
+)
 
 class Job(BaseModel):
     name: str
@@ -26,8 +45,50 @@ class Job(BaseModel):
 def on_startup():
     db.initialize()
 
+def run_docker(new_job):
+    with Session(db.engine) as session:
+        job = session.exec(
+            select(db.Job)
+            .where(col(db.Job.id) == new_job.id)
+        ).one()
+        job.started_at = datetime.datetime.now()
+        session.commit()
+        session.refresh(job)
+
+    container = client4.containers.run(
+        name=new_job.name+"-"+str(new_job.id),
+        image=new_job.container_image, 
+        command=new_job.command,
+        shm_size="1G",
+        detach=True,
+        environment={
+            "NVIDIA_VISIBLE_DEVICES": "0,1",
+        }
+    )
+
+    success = container.wait()
+
+    # if error occurs inside the docker
+    if success != 0:
+        with Session(db.engine) as session:
+            # find if the job exists
+            job = session.exec(
+                select(db.Job)
+                .where(col(db.Job.id) == new_job.id)
+            ).one()
+            job.completed_at = datetime.datetime.now()
+            session.commit()
+            session.refresh(job)
+            
+        # save the output in the `output` folder
+        with open(f"output/{job.name}_{str(job.id)}.txt", "w") as f:
+            # convert the output from bytes to string and write to file
+            f.write(container.logs().decode("utf-8"))
+
+    return success
+
 @app.post("/jobs", response_model=Job)
-async def add_job(job: Job):
+async def add_job(job: Job, background_tasks: BackgroundTasks):
     "Add a job to the scheduler"
     with Session(db.engine) as session:
         job = db.Job(**job.dict())
@@ -36,7 +97,8 @@ async def add_job(job: Job):
         session.refresh(job)
     
     # run the job in the background
-    run_horovod.delay(job.id, job.name, job.container_image, job.command)
+    # run_horovod.delay(job.id, job.name, job.container_image, job.command)
+    background_tasks.add_task(run_docker, job)
     return job
 
 
