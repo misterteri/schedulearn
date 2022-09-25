0f70579a29b1ae99e87edc52fd0ffb52ee554045
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-09-25 23:47:37 +0800

    chore: add docker client address

--- api/main.py
+++ api/main.py
@@ -1,37 +1,31 @@
-import logging
-from logging.config import dictConfig
-from dotenv import load_dotenv
 import config
-import docker
-import datetime
-import database as db
-from fastapi import BackgroundTasks, FastAPI
-from pydantic import BaseModel
-from sqlmodel import Session, select, col
 import uvicorn
+import logging
+
+import database as db
+from schedulearn import Run
+from pydantic import EmailStr, BaseModel
+from dotenv import load_dotenv
+from logging.config import dictConfig
+from sqlmodel import Session, select, col
+from fastapi import FastAPI, HTTPException, BackgroundTasks, WebSocket
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from auth import hash_password, encode_token, verify_password
+from lib import get_docker_client
 
 load_dotenv()
 dictConfig(config.LOGGING)
-
 logger = logging.getLogger("schedulearn")
 app = FastAPI(debug=True)
 
-client3 = docker.DockerClient(
-    base_url=config.GPU3_DOCKER_HOST,
-    use_ssh_client=True,
-    tls=True,
-)
 
-client4 = docker.DockerClient(
-    base_url=config.GPU4_DOCKER_HOST,
-    use_ssh_client=True,
-    tls=True,
-)
-
-client5 = docker.DockerClient(
-    base_url=config.GPU5_DOCKER_HOST,
-    use_ssh_client=True,
-    tls=True
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class Job(BaseModel):
@@ -39,55 +33,84 @@ class Job(BaseModel):
     type: str
     container_image: str
     command: str
-    no_of_gpus: int
+    required_gpus: int
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
 
 @app.on_event("startup")
 def on_startup():
     db.initialize()
 
-def run_docker(new_job):
+@app.post("/signup")
+async def signup(user: User):
+    # check if the email contains `@gapp.nthu.edu.tw`, `@cs.nthu.edu.tw`, or `@office365.nthu.edu.tw`
+    # if not, return 400
+    # if yes, check if the email is in the database
+    domains = ["@gapp.nthu.edu.tw", "@cs.nthu.edu.tw", "@office365.nthu.edu.tw"]
+    if not any(user.email.endswith(domain) for domain in domains):
+        logging.warning("Invalid email")
+        raise HTTPException(status_code=400, detail="Invalid email")
+
     with Session(db.engine) as session:
-        job = session.exec(
-            select(db.Job)
-            .where(col(db.Job.id) == new_job.id)
-        ).one()
-        job.started_at = datetime.datetime.now()
+        if session.get(db.User, user.email):
+            logging.warning("Email already exists")
+            raise HTTPException(
+                status_code=400, 
+                detail="User already exists"
+            )
+        session.add(
+            db.User(
+                email=user.email, 
+                password=hash_password(user.password)
+            )
+        )
         session.commit()
-        session.refresh(job)
+        logging.info("User created")
+        return JSONResponse(status_code=201, content={"message": "User created successfully"})
 
-    container = client4.containers.run(
-        name=new_job.name+"-"+str(new_job.id),
-        image=new_job.container_image, 
-        command=new_job.command,
-        shm_size="1G",
-        detach=True,
-        environment={
-            "NVIDIA_VISIBLE_DEVICES": "0,1",
-        }
-    )
+@app.post("/signin")
+async def user_login(login_user: User):
+    # check if the email exists in the database
+    # if it does, return the user
+    # if it does not, create a new user and return the user
+    with Session(db.engine) as session:
+        user = session.exec(select(db.User).where(db.User.email == login_user.email)).first()
+        if (user is None) or (not verify_password(login_user.password, user.password)):
+            logging.warning("User does not exist")
+            raise HTTPException(
+                status_code=400, 
+                detail="User does not exist"
+            )
 
-    success = container.wait()
+        user.token = encode_token(user.email)
+        session.commit()
+        session.refresh(user)
+        logging.info("A token created")
+    
+    return JSONResponse(status_code=200, content={"username": user.name, "email": user.email, "token": user.token})
 
-    # if error occurs inside the docker
-    if success != 0:
-        with Session(db.engine) as session:
-            # find if the job exists
-            job = session.exec(
-                select(db.Job)
-                .where(col(db.Job.id) == new_job.id)
-            ).one()
-            job.completed_at = datetime.datetime.now()
-            session.commit()
-            session.refresh(job)
-            
-        # save the output in the `output` folder
-        with open(f"output/{job.name}_{str(job.id)}.txt", "w") as f:
-            # convert the output from bytes to string and write to file
-            f.write(container.logs().decode("utf-8"))
+# user sign out
+@app.post("/signout")
+async def user_logout(user: User):
+    with Session(db.engine) as session:
+        user = session.exec(select(db.User).where(db.User.email == user.email)).first()
+        if user is None:
+            logging.warning("User does not exist")
+            raise HTTPException(
+                status_code=400, 
+                detail="User does not exist"
+            )
 
-    return success
+        user.token = None
+        session.commit()
+        logging.info("A token deleted")
+    
+    return JSONResponse(status_code=200, content={"message": "User logged out successfully"})
 
-@app.post("/jobs", response_model=Job)
+
+@app.post("/jobs", response_model=Job, status_code=201)
 async def add_job(job: Job, background_tasks: BackgroundTasks):
     "Add a job to the scheduler"
     with Session(db.engine) as session:
@@ -95,11 +118,9 @@ async def add_job(job: Job, background_tasks: BackgroundTasks):
         session.add(job)
         session.commit()
         session.refresh(job)
-    
-    # run the job in the background
-    # run_horovod.delay(job.id, job.name, job.container_image, job.command)
-    background_tasks.add_task(run_docker, job)
-    return job
+
+    background_tasks.add_task(Run, job)
+    return JSONResponse(status_code=201, content={"message": "Job created successfully"})
 
 
 @app.get("/jobs")
@@ -108,7 +129,7 @@ async def get_jobs():
     with Session(db.engine) as session:
         jobs = session.exec(
             select(db.Job)
-            .order_by(col(db.Job.created_at), col(db.Job.no_of_gpus).desc())
+            .order_by(col(db.Job.created_at).desc())
         ).fetchall()
         return jobs
 
@@ -123,9 +144,26 @@ async def get_job(id: int):
         ).one()
         return job
 
+@app.websocket("/jobs/{id}/logs")
+async def get_job_logs(websocket: WebSocket, id: int):
+    await websocket.accept()
+    with Session(db.engine) as session:
+        job = session.exec(
+            select(db.Job)
+            .where(col(db.Job.id) == id)
+        ).one()
+    
+    docker_client = get_docker_client(job.trained_at)
+    container = docker_client.containers.get(f"{job.name}-{job.id}".lower().replace(" ", "-"))
+    while True:
+        for line in container.logs(stream=True, follow=True):
+            await websocket.send_text(line.decode("ascii"))
+        break
+    
+    await websocket.close()
 
-@app.delete("/jobs/{id}")
-async def kill_job(id: int):
+@app.delete("/jobs/{id}", status_code=204)
+async def kill_job(id: int, background_tasks: BackgroundTasks):
     """
     If a model is on progress, delete the pod immediately, as well as the
     metadata of a model in the database.
@@ -142,7 +180,9 @@ async def kill_job(id: int):
         # delete the job from the database
         session.delete(job)
         session.commit()
-        return job
+    
+    # kill the job in the background
+    return job
 
 
 if __name__ == "__main__":
--- app/pages/jobs/[id].tsx
+++ app/pages/jobs/[id].tsx
@@ -1,16 +1,56 @@
-import Layout from "../../components/Layout";
-import { Box, Heading } from "@chakra-ui/react";
+import Layout from "../../components/Layout/Dashboard";
+import { useEffect, useState } from "react";
+import { Box, Code, Heading, useColorModeValue, VStack } from "@chakra-ui/react";
 
 export default function JobOverview({ job }: { job: any }) {
+  const [output, setOutput] = useState<string[]>([]);
+  const hoverColor = useColorModeValue("gray.200", "gray.500")
+
+  // fetch docker output with useEffect
+  const fetchDockerOutput = useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:5000/jobs/${job.id}/logs`);
+    ws.onmessage = (event: any) => {
+      setOutput((test: string[]) => [...test, event.data]);
+    }
+  }, []);
+
   return (
     <Layout>
       <Box>
-        <Heading as="h1" >{job.name}</Heading>
+        <Heading as="h1" fontSize="2xl">{job.name}</Heading>
         <p>Type: {job.type}</p>
-        <p>Number of required GPUs: {job.no_of_gpus}</p>
+        <p>Number of required GPUs: {job.required_gpus}</p>
         <p>Container image: {job.container_image}</p>
         <p>Command: {job.command}</p>
       </Box>
+
+      <Heading as="h2" mt={10} mb={5}>Training Status</Heading>
+      <details
+        onClick={() => fetchDockerOutput}
+        style={{
+          border: "1px solid #ccc",
+          borderRadius: "0.5rem",
+        }}
+      >
+        <summary style={{ padding: "1rem", marginLeft: "0.5rem" }}>Output</summary>
+        <VStack
+          align="flex-start"
+          spacing={1}
+        >
+          {output.map((line, index) => (
+            <Code
+              key={index}
+              bg="none"
+              _hover={{
+                bg: hoverColor
+              }}
+              width="100%"
+              fontSize="md"
+              px={5}
+            >{line}</Code>
+          ))}
+        </VStack>
+      </details>
     </Layout>
   )
 }
@@ -26,11 +66,8 @@ export const getStaticPaths = async () => {
 }
 
 export const getStaticProps = async ({ params: { id } }: { params: { id: string } }) => {
-  const res = await fetch(`http://localhost:5000/jobs`);
-  const jobs = await res.json();
-
-  // get the job object with the same id as the id in the params
-  const job = jobs.find((job: any) => job.id === Number(id));
+  const res = await fetch(`http://localhost:5000/jobs/${id}`);
+  const job = await res.json();
 
   return { props: { job } };
 }
