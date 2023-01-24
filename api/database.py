a989bb03e0b252eaa65a2b4ea2a8a3bb5bf7f3dd
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-01-24 15:43:05 +0800

    fix: modify database schema

--- api/database.py
+++ api/database.py
@@ -43,8 +43,6 @@ class Job(SQLModel, table=True):
     weight: int = Field(default=None)
     no_of_migrations: int = Field(default=None)
 
-    running_jobs: List["RunningJob"] = Relationship(back_populates="job")
-
 
 class Server(SQLModel, table=True):
     "Keeps track of the servers in the cluster"
@@ -57,24 +55,11 @@ class Server(SQLModel, table=True):
 class Gpu(SQLModel, table=True):
     "Keeps track of the GPU's unique name and the server it is connected to"
     id: Optional[int] = Field(primary_key=True, default=None)
-    identifier: str = Field()
-    
-    server_id: Optional[int] = Field(default=None, foreign_key="server.id")
-    server: Optional[Server] = Relationship(back_populates="gpus")
-
-
-class RunningJob(SQLModel, table=True, table_name="runningjob"):
-    "Record of a job that is currently running"
-    id: Optional[int] = Field(primary_key=True, default=None)
-    started_at: Optional[datetime] = Field(default=None)
-    completed_at: Optional[datetime] = Field(default=None)
-    weight: int = Field(default=None)
-    no_of_migrations: int = Field(default=None)
+    identifier: str = Field(default=None)
+    occupied: bool = Field(default=False)
 
     job_id: Optional[int] = Field(default=None, foreign_key="job.id")
-    gpu_id: Optional[str] = Field(default=None)
-    jobs: List["Job"] = Relationship(back_populates="runningjob")
-    gpus: List["Gpu"] = Relationship(back_populates="runningjob")
+    server_id: Optional[int] = Field(default=None, foreign_key="server.id")
 
 
 def initialize():
